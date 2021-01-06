bb9a3b7ff9a677a3d63489aa4b2e3baba8967f8b
rahgangw <rahgangw@192.168.1.100>
2021-01-06 19:20:39 +0530

    Latest Commit.

--- ContentAutomationFramework/src/test/java/config/ActionKeywords.java
+++ ContentAutomationFramework/src/test/java/config/ActionKeywords.java
@@ -43,156 +43,243 @@ import executionEngine.ConfigReader;
 import executionEngine.DriverScript;
 import utility.ExcelUtils;
 
+
 public class ActionKeywords {
 
 	public static WebDriver driver = null;
 	static ConfigReader configReader = new ConfigReader();
-    public static String ActualText;
+	public static String ActualText;
 
-
-
-    
 	public void openBrowser() throws Exception {
 		try {
-				System.setProperty("webdriver.chrome.driver", Constants.Driver_Path_Chrome);
-				driver = new ChromeDriver();
-				driver.manage().window().maximize();
-				int implicitWaitTime = (10);
-			    driver.manage().timeouts().implicitlyWait(implicitWaitTime, TimeUnit.SECONDS);
+			System.setProperty("webdriver.chrome.driver", Constants.Driver_Path_Chrome);
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			int implicitWaitTime = (10);
+			driver.manage().timeouts().implicitlyWait(implicitWaitTime, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			DriverScript.bResult = false;
-			
+
 		}
 
 	}
 
-	public void navigate() throws Exception{
+	public void navigate() throws Exception {
 		try {
-			
+
 			driver.get(configReader.getApplicationUrl());
 			WebDriverWait wait = new WebDriverWait(driver, 20);
-			
-			
+
 		} catch (Exception e) {
 			DriverScript.bResult = false;
-			
+
 		}
-	
+
 	}
-	
-	public void ourFirm() throws Exception{
+
+	public void ourFirm() throws Exception {
 		try {
-			
+
 			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
 			driver.findElement(By.linkText("Our firm")).click();
 			WebDriverWait wait = new WebDriverWait(driver, 20);
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]")).click();
-			
-			
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]"))
+					.click();
+
 		} catch (Exception e) {
 			DriverScript.bResult = false;
-			
+
 		}
-	
+
 	}
 
-	public void ourProjects() throws Exception{
+	public void ourProjects() throws Exception {
 		try {
-			
+
 			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
 			driver.findElement(By.linkText("Our projects")).click();
 			WebDriverWait wait = new WebDriverWait(driver, 20);
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]")).click();
-			
-			
-		} catch (Exception e) {
-			DriverScript.bResult = false;
-			
-		}
-	
-	}
-	
-	public void ourServices() throws Exception{
-		try {
-			
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
-			driver.findElement(By.linkText("Our services")).click();
-			WebDriverWait wait = new WebDriverWait(driver, 20);
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]")).click();
-			
-			
-		} catch (Exception e) {
-			DriverScript.bResult = false;
-			
-		}
-	
-	}
-	
-	public void insights() throws Exception{
-		try {
-			
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
-			driver.findElement(By.linkText("Insights")).click();
-			WebDriverWait wait = new WebDriverWait(driver, 20);
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]")).click();
-			
-			
-		} catch (Exception e) {
-			DriverScript.bResult = false;
-			
-		}
-	
-	}
-	
-	public void contactUs() throws Exception{
-		try {
-			
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
-			driver.findElement(By.linkText("Contact us")).click();
-			WebDriverWait wait = new WebDriverWait(driver, 20);
-			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]")).click();
-			
-			
-		} catch (Exception e) {
-			DriverScript.bResult = false;
-			
-		}
-	
-	}
-	
-	public void fetchText(String object, String ActualText) throws InterruptedException {
-		try {
-			WebElement FT = driver.findElement(By.xpath(OR.getProperty(object)));
-            //System.out.println(FT.getText());
-            this.ActualText = FT.getText();
-            
-            ExcelUtils.setCellData(this.ActualText, DriverScript.iHomePage, Constants.Col_ActualContent,
-					Constants.Sheet_HomePage);
-            
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]"))
+					.click();
 
 		} catch (Exception e) {
 			DriverScript.bResult = false;
-			
+
 		}
-       
+
 	}
-	
-	public void pageUrl()
-	{
-			
+
+	public void ourServices() throws Exception {
+		try {
+
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
+			driver.findElement(By.linkText("Our services")).click();
+			WebDriverWait wait = new WebDriverWait(driver, 20);
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]"))
+					.click();
+
+		} catch (Exception e) {
+			DriverScript.bResult = false;
+
+		}
+
+	}
+
+	public void insights() throws Exception {
+		try {
+
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
+			driver.findElement(By.linkText("Insights")).click();
+			WebDriverWait wait = new WebDriverWait(driver, 20);
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]"))
+					.click();
+
+		} catch (Exception e) {
+			DriverScript.bResult = false;
+
+		}
+
+	}
+
+	public void contactUs() throws Exception {
+		try {
+
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/div[2]/div")).click();
+			driver.findElement(By.linkText("Contact us")).click();
+			WebDriverWait wait = new WebDriverWait(driver, 20);
+			driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/header/div[1]/div/a[2]/span/span[2]/svg[2]"))
+					.click();
+
+		} catch (Exception e) {
+			DriverScript.bResult = false;
+
+		}
+
+	}
+
+	public void fetchText(String object, String ActualText) throws InterruptedException {
+		try {
+
+			// System.out.println(driver.getCurrentUrl());
+			String CurrentUrl = driver.getCurrentUrl();
+
+			if (CurrentUrl.contains("our-firm"))
+
+			{
+				WebElement FT = driver.findElement(By.xpath(OR.getProperty(object)));
+
+				Actions actions = new Actions(driver);
+				actions.moveToElement(FT);
+				actions.perform();
+				Thread.sleep(3000);
+
+				this.ActualText = FT.getText();
+
+				ExcelUtils.setCellData(this.ActualText, DriverScript.iOurFirm, Constants.Col_ActualContent,
+						Constants.Sheet_OurFirmPage);
+
+			}
+
+			else if (CurrentUrl.contains("projects"))
+
+			{
+				WebElement FT = driver.findElement(By.xpath(OR.getProperty(object)));
+
+				Actions actions = new Actions(driver);
+				actions.moveToElement(FT);
+				actions.perform();
+				Thread.sleep(3000);
+
+				this.ActualText = FT.getText();
+
+				ExcelUtils.setCellData(this.ActualText, DriverScript.iProjects, Constants.Col_ActualContent,
+						Constants.Sheet_OurProjectsPage);
+			}
+
+			else if (CurrentUrl.contains("our-services"))
+
+			{
+				WebElement FT = driver.findElement(By.xpath(OR.getProperty(object)));
+
+				Actions actions = new Actions(driver);
+				actions.moveToElement(FT);
+				actions.perform();
+				Thread.sleep(3000);
+
+				this.ActualText = FT.getText();
+
+				ExcelUtils.setCellData(this.ActualText, DriverScript.iOurServices, Constants.Col_ActualContent,
+						Constants.Sheet_OurServicesPage);
+			}
+
+			else if (CurrentUrl.contains("insights"))
+
+			{
+				WebElement FT = driver.findElement(By.xpath(OR.getProperty(object)));
+
+				Actions actions = new Actions(driver);
+				actions.moveToElement(FT);
+				actions.perform();
+				Thread.sleep(3000);
+
+				this.ActualText = FT.getText();
+
+				ExcelUtils.setCellData(this.ActualText, DriverScript.iInsights, Constants.Col_ActualContent,
+						Constants.Sheet_InsightsPage);
+			}
+
+			else if (CurrentUrl.contains("contact-us"))
+
+			{
+				WebElement FT = driver.findElement(By.xpath(OR.getProperty(object)));
+
+				Actions actions = new Actions(driver);
+				actions.moveToElement(FT);
+				actions.perform();
+				Thread.sleep(3000);
+
+				this.ActualText = FT.getText();
+
+				ExcelUtils.setCellData(this.ActualText, DriverScript.iContactUs, Constants.Col_ActualContent,
+						Constants.Sheet_ContactUsPage);
+			}
+
+			else {
+
+				WebElement FT = driver.findElement(By.xpath(OR.getProperty(object)));
+
+				Actions actions = new Actions(driver);
+				actions.moveToElement(FT);
+				actions.perform();
+				Thread.sleep(3000);
+
+				this.ActualText = FT.getText();
+
+				ExcelUtils.setCellData(this.ActualText, DriverScript.iHomePage, Constants.Col_ActualContent,
+						Constants.Sheet_HomePage);
+			}
+
+		} catch (Exception e) {
+			DriverScript.bResult = false;
+
+		}
+
+	}
+
+	public void pageUrl() {
+
 		System.out.println(driver.getCurrentUrl());
-		
+
 	}
 
 	public void closeBrowser() {
 		try {
-	      //System.out.println(ActionKeywords.ActualText);
+			// System.out.println(ActionKeywords.ActualText);
 			driver.quit();
-			   
+
 		} catch (AssertionError e) {
 			DriverScript.bResult = false;
 		}
 	}
-	
 
 }
--- ContentAutomationFramework/src/test/java/executionEngine/DriverScript.java
+++ ContentAutomationFramework/src/test/java/executionEngine/DriverScript.java
@@ -26,7 +26,6 @@ import config.ActionKeywords;
 import config.Constants;
 import utility.ExcelUtils;
 
-
 public class DriverScript extends ActionKeywords {
 
 	ConfigReader configReader;
@@ -48,182 +47,322 @@ public class DriverScript extends ActionKeywords {
 	public static boolean bResult;
 	public static String sData;
 	public static String sData1;
-	
+
 	static int iTestcase = 1;
 
 	public DriverScript() {
 
 		actionKeywords = new ActionKeywords();
-		method = actionKeywords.getClass().getMethods();	
-		
+		method = actionKeywords.getClass().getMethods();
+
 	}
-	
 
 	public void execute_TestCase() throws Exception {
-		
+
 		ExcelUtils.setExcelFile(Constants.Path_TestData);
-		
+
 		String Path_OR = Constants.Path_OR;
 		FileInputStream fs = new FileInputStream(Path_OR);
 		OR = new Properties(System.getProperties());
 		OR.load(fs);
-		
-		
-			System.out.println(driver.getCurrentUrl());
-			String CurrentUrl = driver.getCurrentUrl();
-			
-			if(CurrentUrl.contains("our-firm"))
-			
-               {
-				
-				//	int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_OurFirmPage);
-					
 
-						sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName, Constants.Sheet_OurFirmPage);
-							
-				iOurFirm = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName, Constants.Sheet_OurFirmPage);
-				iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_OurFirmPage, sWebPageName, iOurFirm);
+		System.out.println(driver.getCurrentUrl());
+		String CurrentUrl = driver.getCurrentUrl();
 
-				for (; iOurFirm < iTestLastStep; iOurFirm++) {
-					sActionKeyword = ExcelUtils.getCellData(iOurFirm, Constants.Col_ActionKeyword,
-							Constants.Sheet_OurFirmPage);
-					sPageObject = ExcelUtils.getCellData(iOurFirm, Constants.Col_Component,
-							Constants.Sheet_OurFirmPage);
-					
-					for (int i = 0; i < method.length; i++) {
+		if (CurrentUrl.contains("our-firm"))
 
-						if (method[i].getName().equals(sActionKeyword)) {
+		{
 
-							method[i].invoke(actionKeywords, sPageObject, sData);
-						
-							
-							sData = ExcelUtils.getCellData(iOurFirm, Constants.Col_ExpectedContent, Constants.Sheet_OurFirmPage);
-							System.out.println(sData);
-							
-							sData1 = ExcelUtils.getCellData(iOurFirm, Constants.Col_ActualContent, Constants.Sheet_OurFirmPage);
-							System.out.println(sData1);
-							
-							if (sData.equals(sData1)) {
-								ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iOurFirm, Constants.Col_ContentMatched,
-										Constants.Sheet_OurFirmPage);
-								break;
-							} else {
-								ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iOurFirm, Constants.Col_ContentMatched,
-										Constants.Sheet_OurFirmPage);
-								//ActionKeywords.closeBrowser();
-								break;
+			// int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_OurFirmPage);
 
-							}
+			sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName, Constants.Sheet_OurFirmPage);
+
+			iOurFirm = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName, Constants.Sheet_OurFirmPage);
+			iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_OurFirmPage, sWebPageName, iOurFirm);
+
+			for (; iOurFirm < iTestLastStep; iOurFirm++) {
+				sActionKeyword = ExcelUtils.getCellData(iOurFirm, Constants.Col_ActionKeyword,
+						Constants.Sheet_OurFirmPage);
+				sPageObject = ExcelUtils.getCellData(iOurFirm, Constants.Col_Component, Constants.Sheet_OurFirmPage);
+
+				for (int i = 0; i < method.length; i++) {
+
+					if (method[i].getName().equals(sActionKeyword)) {
+
+						method[i].invoke(actionKeywords, sPageObject, sData);
+
+						sData = ExcelUtils.getCellData(iOurFirm, Constants.Col_ExpectedContent,
+								Constants.Sheet_OurFirmPage);
+						System.out.println(sData);
+
+						sData1 = ExcelUtils.getCellData(iOurFirm, Constants.Col_ActualContent,
+								Constants.Sheet_OurFirmPage);
+						System.out.println(sData1);
+
+						if (sData.equals(sData1)) {
+							ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iOurFirm, Constants.Col_ContentMatched,
+									Constants.Sheet_OurFirmPage);
+							break;
+						} else {
+							ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iOurFirm,
+									Constants.Col_ContentMatched, Constants.Sheet_OurFirmPage);
+							// ActionKeywords.closeBrowser();
+							break;
+
 						}
 					}
-				
 				}
-					
-               }
-				
-			else if(CurrentUrl.contains("our-projects"))
-				
-            {
-				
-				//	int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_OurFirmPage);
-					
 
-						sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName, Constants.Sheet_OurProjectsPage);
-							
-				iProjects = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName, Constants.Sheet_OurProjectsPage);
-				iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_OurProjectsPage, sWebPageName, iProjects);
+			}
 
-				for (; iProjects < iTestLastStep; iProjects++) {
-					sActionKeyword = ExcelUtils.getCellData(iProjects, Constants.Col_ActionKeyword,
-							Constants.Sheet_OurProjectsPage);
-					sPageObject = ExcelUtils.getCellData(iProjects, Constants.Col_Component,
-							Constants.Sheet_OurProjectsPage);
-					
-					for (int i = 0; i < method.length; i++) {
-
-						if (method[i].getName().equals(sActionKeyword)) {
-
-							method[i].invoke(actionKeywords, sPageObject, sData);
-						
-							
-							sData = ExcelUtils.getCellData(iProjects, Constants.Col_ExpectedContent, Constants.Sheet_OurProjectsPage);
-							System.out.println(sData);
-							
-							sData1 = ExcelUtils.getCellData(iProjects, Constants.Col_ActualContent, Constants.Sheet_OurProjectsPage);
-							System.out.println(sData1);
-							
-							if (sData.equals(sData1)) {
-								ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iProjects, Constants.Col_ContentMatched,
-										Constants.Sheet_OurProjectsPage);
-								break;
-							} else {
-								ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iProjects, Constants.Col_ContentMatched,
-										Constants.Sheet_OurProjectsPage);
-								//ActionKeywords.closeBrowser();
-								break;
-
-							}
-						}
-					}
-				
-				}
-					
-            }
-				else
-				{
-					
-				//	int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_HomePage);
-				//	for (int iTestcase = 1; iTestcase < iTotalTestCases; iTestcase++) {
-
-						sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName, Constants.Sheet_HomePage);
-						
-						
-					iHomePage = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName, Constants.Sheet_HomePage);
-					iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_HomePage, sWebPageName, iHomePage);
-
-					for (; iHomePage < iTestLastStep; iHomePage++) {
-						sActionKeyword = ExcelUtils.getCellData(iHomePage, Constants.Col_ActionKeyword,
-								Constants.Sheet_HomePage);
-						sPageObject = ExcelUtils.getCellData(iHomePage, Constants.Col_Component,
-								Constants.Sheet_HomePage);
-						
-						for (int i = 0; i < method.length; i++) {
-
-							if (method[i].getName().equals(sActionKeyword)) {
-
-								method[i].invoke(actionKeywords, sPageObject, sData);
-							
-								
-								sData = ExcelUtils.getCellData(iHomePage, Constants.Col_ExpectedContent, Constants.Sheet_HomePage);
-								System.out.println(sData);
-								
-								sData1 = ExcelUtils.getCellData(iHomePage, Constants.Col_ActualContent, Constants.Sheet_HomePage);
-								System.out.println(sData1);
-								
-								if (sData.equals(sData1)) {
-									ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iHomePage, Constants.Col_ContentMatched,
-											Constants.Sheet_HomePage);
-									break;
-								} else {
-									ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iHomePage, Constants.Col_ContentMatched,
-											Constants.Sheet_HomePage);
-									//ActionKeywords.closeBrowser();
-									break;
-
-								}
-							}
-						}
-				}
-	//			if (sData.equals(sData1)) {
-	//				ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iTestcase, Constants.Col_Result,
-	//						Constants.Sheet_Summary);
-
-				
-
-			
-				}
-				
 		}
-	
+
+		else if (CurrentUrl.contains("projects"))
+
+		{
+
+			// int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_OurFirmPage);
+
+			sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName,
+					Constants.Sheet_OurProjectsPage);
+
+			iProjects = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName,
+					Constants.Sheet_OurProjectsPage);
+			iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_OurProjectsPage, sWebPageName, iProjects);
+
+			for (; iProjects < iTestLastStep; iProjects++) {
+				sActionKeyword = ExcelUtils.getCellData(iProjects, Constants.Col_ActionKeyword,
+						Constants.Sheet_OurProjectsPage);
+				sPageObject = ExcelUtils.getCellData(iProjects, Constants.Col_Component,
+						Constants.Sheet_OurProjectsPage);
+
+				for (int i = 0; i < method.length; i++) {
+
+					if (method[i].getName().equals(sActionKeyword)) {
+
+						method[i].invoke(actionKeywords, sPageObject, sData);
+
+						sData = ExcelUtils.getCellData(iProjects, Constants.Col_ExpectedContent,
+								Constants.Sheet_OurProjectsPage);
+						System.out.println(sData);
+
+						sData1 = ExcelUtils.getCellData(iProjects, Constants.Col_ActualContent,
+								Constants.Sheet_OurProjectsPage);
+						System.out.println(sData1);
+
+						if (sData.equals(sData1)) {
+							ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iProjects, Constants.Col_ContentMatched,
+									Constants.Sheet_OurProjectsPage);
+							break;
+						} else {
+							ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iProjects,
+									Constants.Col_ContentMatched, Constants.Sheet_OurProjectsPage);
+							// ActionKeywords.closeBrowser();
+							break;
+
+						}
+					}
+				}
+
+			}
+		} 
+		
+		else if (CurrentUrl.contains("our-services"))
+
+		{
+
+			// int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_OurFirmPage);
+
+			sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName,
+					Constants.Sheet_OurServicesPage);
+
+			iOurServices = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName,
+					Constants.Sheet_OurServicesPage);
+			iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_OurServicesPage, sWebPageName, iOurServices);
+
+			for (; iOurServices < iTestLastStep; iOurServices++) {
+				sActionKeyword = ExcelUtils.getCellData(iOurServices, Constants.Col_ActionKeyword,
+						Constants.Sheet_OurServicesPage);
+				sPageObject = ExcelUtils.getCellData(iOurServices, Constants.Col_Component,
+						Constants.Sheet_OurServicesPage);
+
+				for (int i = 0; i < method.length; i++) {
+
+					if (method[i].getName().equals(sActionKeyword)) {
+
+						method[i].invoke(actionKeywords, sPageObject, sData);
+
+						sData = ExcelUtils.getCellData(iOurServices, Constants.Col_ExpectedContent,
+								Constants.Sheet_OurServicesPage);
+						System.out.println(sData);
+
+						sData1 = ExcelUtils.getCellData(iOurServices, Constants.Col_ActualContent,
+								Constants.Sheet_OurServicesPage);
+						System.out.println(sData1);
+
+						if (sData.equals(sData1)) {
+							ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iOurServices,
+									Constants.Col_ContentMatched, Constants.Sheet_OurServicesPage);
+							break;
+						} else {
+							ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iOurServices,
+									Constants.Col_ContentMatched, Constants.Sheet_OurServicesPage);
+							// ActionKeywords.closeBrowser();
+							break;
+
+						}
+					}
+				}
+
+			}
+
+		} 
+		
+		else if (CurrentUrl.contains("insights"))
+
+		{
+
+			// int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_OurFirmPage);
+
+			sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName, Constants.Sheet_InsightsPage);
+
+			iInsights = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName,
+					Constants.Sheet_InsightsPage);
+			iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_InsightsPage, sWebPageName, iInsights);
+
+			for (; iInsights < iTestLastStep; iInsights++) {
+				sActionKeyword = ExcelUtils.getCellData(iInsights, Constants.Col_ActionKeyword,
+						Constants.Sheet_InsightsPage);
+				sPageObject = ExcelUtils.getCellData(iInsights, Constants.Col_Component, Constants.Sheet_InsightsPage);
+
+				for (int i = 0; i < method.length; i++) {
+
+					if (method[i].getName().equals(sActionKeyword)) {
+
+						method[i].invoke(actionKeywords, sPageObject, sData);
+
+						sData = ExcelUtils.getCellData(iInsights, Constants.Col_ExpectedContent,
+								Constants.Sheet_InsightsPage);
+						System.out.println(sData);
+
+						sData1 = ExcelUtils.getCellData(iInsights, Constants.Col_ActualContent,
+								Constants.Sheet_InsightsPage);
+						System.out.println(sData1);
+
+						if (sData.equals(sData1)) {
+							ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iInsights, Constants.Col_ContentMatched,
+									Constants.Sheet_InsightsPage);
+							break;
+						} else {
+							ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iInsights,
+									Constants.Col_ContentMatched, Constants.Sheet_InsightsPage);
+							// ActionKeywords.closeBrowser();
+							break;
+
+						}
+					}
+				}
+
+			}
+
+		} 
+		
+		else if (CurrentUrl.contains("contact-us"))
+
+		{
+
+			// int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_OurFirmPage);
+
+			sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName, Constants.Sheet_ContactUsPage);
+
+			iContactUs = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName,
+					Constants.Sheet_ContactUsPage);
+			iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_ContactUsPage, sWebPageName, iContactUs);
+
+			for (; iContactUs < iTestLastStep; iContactUs++) {
+				sActionKeyword = ExcelUtils.getCellData(iContactUs, Constants.Col_ActionKeyword,
+						Constants.Sheet_ContactUsPage);
+				sPageObject = ExcelUtils.getCellData(iContactUs, Constants.Col_Component,
+						Constants.Sheet_ContactUsPage);
+
+				for (int i = 0; i < method.length; i++) {
+
+					if (method[i].getName().equals(sActionKeyword)) {
+
+						method[i].invoke(actionKeywords, sPageObject, sData);
+
+						sData = ExcelUtils.getCellData(iContactUs, Constants.Col_ExpectedContent,
+								Constants.Sheet_ContactUsPage);
+						System.out.println(sData);
+
+						sData1 = ExcelUtils.getCellData(iContactUs, Constants.Col_ActualContent,
+								Constants.Sheet_ContactUsPage);
+						System.out.println(sData1);
+
+						if (sData.equals(sData1)) {
+							ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iContactUs, Constants.Col_ContentMatched,
+									Constants.Sheet_ContactUsPage);
+							break;
+						} else {
+							ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iContactUs,
+									Constants.Col_ContentMatched, Constants.Sheet_ContactUsPage);
+							// ActionKeywords.closeBrowser();
+							break;
+
+						}
+					}
+				}
+
+			}
+
+		} 
+		
+		else {
+
+			// int iTotalTestCases = ExcelUtils.getRowCount(Constants.Sheet_HomePage);
+			// for (int iTestcase = 1; iTestcase < iTotalTestCases; iTestcase++) {
+
+			sWebPageName = ExcelUtils.getCellData(iTestcase, Constants.Col_WebPageName, Constants.Sheet_HomePage);
+
+			iHomePage = ExcelUtils.getRowContains(sWebPageName, Constants.Col_WebPageName, Constants.Sheet_HomePage);
+			iTestLastStep = ExcelUtils.getTestStepsCount(Constants.Sheet_HomePage, sWebPageName, iHomePage);
+
+			for (; iHomePage < iTestLastStep; iHomePage++) {
+				sActionKeyword = ExcelUtils.getCellData(iHomePage, Constants.Col_ActionKeyword,
+						Constants.Sheet_HomePage);
+				sPageObject = ExcelUtils.getCellData(iHomePage, Constants.Col_Component, Constants.Sheet_HomePage);
+
+				for (int i = 0; i < method.length; i++) {
+
+					if (method[i].getName().equals(sActionKeyword)) {
+
+						method[i].invoke(actionKeywords, sPageObject, sData);
+
+						sData = ExcelUtils.getCellData(iHomePage, Constants.Col_ExpectedContent,
+								Constants.Sheet_HomePage);
+						System.out.println(sData);
+
+						sData1 = ExcelUtils.getCellData(iHomePage, Constants.Col_ActualContent,
+								Constants.Sheet_HomePage);
+						System.out.println(sData1);
+
+						if (sData.equals(sData1)) {
+							ExcelUtils.setCellData(Constants.KEYWORD_MATCHED, iHomePage, Constants.Col_ContentMatched,
+									Constants.Sheet_HomePage);
+							break;
+						} else {
+							ExcelUtils.setCellData(Constants.KEYWORD_NOT_MATCHED, iHomePage,
+									Constants.Col_ContentMatched, Constants.Sheet_HomePage);
+							// ActionKeywords.closeBrowser();
+							break;
+
+						}
+					}
+				}
+			}
+
+		}
+
+	}
 
 }
